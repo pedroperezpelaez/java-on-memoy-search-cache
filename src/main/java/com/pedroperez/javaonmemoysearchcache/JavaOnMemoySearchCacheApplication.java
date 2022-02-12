2119bd513dd501a9de4ebef3b40f4f624eed56a6
pedro <tineo4ever@gmail.com>
2022-02-12 12:05:10 +0100

    Initial structure

---
+++ src/main/java/com/pedroperez/javaonmemoysearchcache/JavaOnMemoySearchCacheApplication.java
@@ -0,0 +1,13 @@
+package com.pedroperez.javaonmemoysearchcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOnMemoySearchCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOnMemoySearchCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroperez/javaonmemoysearchcache/JavaOnMemoySearchCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroperez.javaonmemoysearchcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOnMemoySearchCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
